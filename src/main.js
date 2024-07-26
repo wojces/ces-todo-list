0d407d24148f8e9f9a6a8e28724d5795dba2bc96
Wojciech <wojtekc321@gmail.com>
2024-07-26 10:40:01 +0200

    changes build 2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
-// import { createApp } from 'vue'
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import store from './store/index.js'
 
-// createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
-  outputDir: 'dist',
-  assetsDir: 'static'
 })
